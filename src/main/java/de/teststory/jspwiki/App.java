34593ff10fb898464517943c8d775817bfcb55dc
Christian Fröhler <Christian.Froehler@gmx.net>
2016-03-19 23:51:57 +0100

    Initial mvn Hello World Project

---
+++ src/main/java/de/teststory/jspwiki/App.java
@@ -0,0 +1,13 @@
+package de.teststory.jspwiki;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
