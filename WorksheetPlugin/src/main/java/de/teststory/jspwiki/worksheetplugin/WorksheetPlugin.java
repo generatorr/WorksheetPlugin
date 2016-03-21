396560209acd300188b4ab25c957676f0245b85d
Christian Fröhler <Christian.Froehler@gmx.net>
2016-03-21 22:25:50 +0100

    - removed log4j.properties - removed dependency to Table plugin - added poi-example to generate html code - changed to Apache License 2.0

--- WorksheetPlugin/src/main/java/de/teststory/jspwiki/worksheetplugin/WorksheetPlugin.java
+++ WorksheetPlugin/src/main/java/de/teststory/jspwiki/worksheetplugin/WorksheetPlugin.java
@@ -1,16 +1,28 @@
+/*
+ * Copyright 2016 Christian Froehler
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.teststory.jspwiki.worksheetplugin;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Map;
-import java.util.regex.Pattern;
 
 import org.apache.log4j.Logger;
 import org.apache.poi.EncryptedDocumentException;
-import org.apache.poi.hssf.usermodel.HSSFDateUtil;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.examples.html.ToHtml;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
@@ -18,13 +30,12 @@ import org.apache.wiki.WikiContext;
 import org.apache.wiki.WikiEngine;
 import org.apache.wiki.api.exceptions.PluginException;
 import org.apache.wiki.api.exceptions.ProviderException;
+import org.apache.wiki.api.plugin.WikiPlugin;
 import org.apache.wiki.attachment.Attachment;
 import org.apache.wiki.attachment.AttachmentManager;
 import org.apache.wiki.util.TextUtil;
 
-import brushed.jspwiki.tableplugin.Table;
-
-public class WorksheetPlugin extends Table {
+public class WorksheetPlugin implements WikiPlugin  {
 
 	/** The parameter name for setting the src. Value is <tt>{@value}</tt>. */
 	public static final String PARAM_SRC = "src";
@@ -32,16 +43,10 @@ public class WorksheetPlugin extends Table {
 	public static final String PARAM_WORKSHEET_ID = "sheetId";
 	/** The parameter name for setting the worksheet_name. Value is <tt>{@value}</tt> */
 	public static final String PARAM_WORKSHEET_NAME = "sheetName";
-	/** The parameter name for showing the wiki source text instead of the table. Value is <tt>{@value}</tt> */
-	public static final String PARAM_SHOW_WIKISOURCE = "showsource";
-
-	private static final String NL = System.getProperty("line.separator");
-	private static final Pattern PIPE_PATTERN = Pattern.compile("\\|");
-	private static final Pattern NL_PATTERN = Pattern.compile("\r?\n");
 
 	private final Logger log = Logger.getLogger(WorksheetPlugin.class);
 	
-	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@SuppressWarnings({ "rawtypes", "unchecked" }) 
 	@Override
 	public String execute(WikiContext context, Map params) throws PluginException {
 		log.info("WorksheetPlugin executed");
@@ -61,8 +66,6 @@ public class WorksheetPlugin extends Table {
 				}
 			}
 			String worksheetName = getCleanParameter(params, PARAM_WORKSHEET_NAME); // may be null
-			s = getCleanParameter(params, PARAM_SHOW_WIKISOURCE);
-			boolean showWikiSource = s != null ? Boolean.valueOf(s).booleanValue() : false;
 
 			// Load the workbook
 			WikiEngine engine = context.getEngine();
@@ -83,17 +86,12 @@ public class WorksheetPlugin extends Table {
 				throw new PluginException(
 						"Could not find Worksheet. Index=" + worksheetId + ", name='" + worksheetName + "'");
 			}
-
-			// Create Wiki table
-			String body = analyzeSheet(sheet);
-
-			if (showWikiSource) {
-				return "<pre>" + body + "</pre>";
-			} else {
-				// Format the table using the "Table" Plugin
-				params.put(PARAM_BODY, body);
-				return super.execute(context, params);
-			}
+			
+			StringBuilder sb = new StringBuilder();
+			ToHtml toHtml = ToHtml.create(wb, sb);
+			toHtml.printSheet(sheet);
+			return sb.toString();
+			
 		} catch (ProviderException e) {
 			throw new PluginException("Attachment info failed: " + e.getMessage(), e);
 		} catch (IOException e) {
@@ -135,109 +133,6 @@ public class WorksheetPlugin extends Table {
 			ret = wb.getSheetAt(0);
 		}
 		return ret;
-	}
-
-	/**
-	 * Transforms the content of the sheet into Wiki Table Syntax, as Input for
-	 * the Table plugin.
-	 * 
-	 * @param sheet
-	 * @return
-	 */
-	protected String analyzeSheet(Sheet sheet) {
-		StringBuilder sb = new StringBuilder();
-
-		int maxRowNum = sheet.getLastRowNum();
-
-		// iterate through rows to find the highest column index
-		int maxColumnNum = 0;
-		for (int rowNum = 0; rowNum <= maxRowNum; ++rowNum) {
-			Row row = sheet.getRow(rowNum);
-			int lastColumnNum = row != null ? row.getLastCellNum() : 0;
-			maxColumnNum = Math.max(maxColumnNum, lastColumnNum);
-		}
-
-		// parse cells and generate wiki markup
-		for (int rowNum = 0; rowNum <= maxRowNum; ++rowNum) {
-			Row row = sheet.getRow(rowNum);
-			if (row == null) {
-				continue;
-			}
-			for (int colNum = 0; colNum <= maxColumnNum; ++colNum) {
-				Cell cell = row.getCell(colNum);
-				sb.append(analyzeCell(cell));
-			}
-			sb.append(NL);
-		}
-
-		return sb.toString();
-	}
-
-	protected String analyzeCell(Cell cell) {
-		StringBuilder ret = new StringBuilder();
-		// cell delimiter
-		ret.append("|");
-
-		// TODO check if cell is within a merged region
-
-		// formats
-		String cellFormat = "";
-		// TODO add css formats
-		ret.append(cellFormat);
-
-		// Text content
-		String cellValue = "";
-		if (cell != null) {
-			int cellType = cell.getCellType();
-			if (cellType == Cell.CELL_TYPE_FORMULA) {
-				cellType = cell.getCachedFormulaResultType();
-			}
-			switch (cellType) {
-			case Cell.CELL_TYPE_BLANK:
-				break;
-			case Cell.CELL_TYPE_BOOLEAN:
-				cellValue = cell.getBooleanCellValue() ? "Yes" : "No";
-				break;
-			case Cell.CELL_TYPE_ERROR:
-				cellValue = "Error code: " + cell.getErrorCellValue();
-				break;
-			case Cell.CELL_TYPE_FORMULA:
-				cellValue = cell.getCellFormula();
-				break;
-			case Cell.CELL_TYPE_NUMERIC:
-				// might be also a date
-				if (HSSFDateUtil.isCellDateFormatted(cell)) {
-					cellValue = String.valueOf(cell.getDateCellValue());
-				} else {
-					cellValue = String.valueOf(cell.getNumericCellValue());
-				}
-				break;
-			case Cell.CELL_TYPE_STRING:
-				cellValue = cell.getStringCellValue();
-				break;
-			default:
-				ret.append("unknown cell type: " + cellType);
-			}
-		}
-		ret.append(cleanCellValue(cellValue));
-		return ret.toString();
-
-	}
-
-	/**
-	 * Replace | and Newline Characters by Wiki markup
-	 * 
-	 * @param in
-	 * @return
-	 */
-	protected static String cleanCellValue(String in) {
-		if (in == null || in.length() == 0) {
-			in = " ";
-		} else {
-			in = NL_PATTERN.matcher(in).replaceAll("\\\\\\\\");
-			in = PIPE_PATTERN.matcher(in).replaceAll("-");
-		}
-		return in;
 	}
 
 }
